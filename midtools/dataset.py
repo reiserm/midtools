6301ef8445164a5cc097bd72fe0c4c6342b7d90d
Mario Reiser <mario.reiser@xfel.eu>
2020-04-30 09:34:11 +0200

    added mask property and docstr

--- midtools/dataset.py
+++ midtools/dataset.py
@@ -5,6 +5,8 @@ import numpy as np
 from extra_data import RunDirectory
 from extra_geom import AGIPD_1MGeometry
 
+from midtools import azimuthal_integration
+
 class Dataset:
     
     def __init__(self, setupfile):
@@ -38,26 +40,32 @@ class Dataset:
               q4: [ 620, 500]
         """
         
-        setup_pars = self.read_setup(setupfile)
-        
+        self.setupfile = setupfile
+        setup_pars = self._read_setup(setupfile)
         
         self.datdir = setup_pars.pop('datdir', False) #: str: Data directory
         #: DataCollection: e.g., returned from extra_data.RunDirectory
         self.run = RunDirectory(self.datdir) 
         
+        self.mask = setup_pars.pop('mask', None)
+        
         #: tuple: Position of the direct beam in pixels
         self.center = None
         
-        #: AGIPD_1MGeometry: 
         self.agipd_geom = setup_pars.pop('quadrant_positions', False)
         self.__dict__.update(setup_pars)
         
-        #: str: Path to the mask-file
-        self.mask = setup_pars.pop('mask', None)
         del setup_pars
+        
+    def __str__(self):
+        return "MID dataset."
+    
+    def __repr(self):
+        return f"Dataset({self.setupfile})"
     
     @property
     def datdir(self):
+        """str: Data directory."""
         return self.__datdir
 
     @datdir.setter
@@ -65,8 +73,12 @@ class Dataset:
         if isinstance(path, dict):
             basedir = '/gpfs/exfel/exp/MID/'
             if len(list(filter(lambda x: x in path.keys(), 
-                               ['cycle', 'proposal', 'datatype']))):
-                path = basedir + f"/{path['cycle']}/p{path['proposal']:06d}/{path['datatype']}"
+                               ['cycle', 'proposal', 'datatype', 'run']))) == 4:
+                path = basedir \
+                    + f"/{path['cycle']}" \
+                    + f"/p{path['proposal']:06d}" \
+                    + f"/{path['datatype']}" \
+                    + f"/r{path['run']:04d}"
         elif isinstance(path, str):
             pass
         else:
@@ -80,6 +92,7 @@ class Dataset:
             
     @property
     def agipd_geom(self):
+        """AGIPD_1MGeometry: AGIPD geometry obtained from extra_data."""
         return self.__agipd_geom
 
     @agipd_geom.setter
@@ -98,8 +111,14 @@ class Dataset:
     
     
     @staticmethod
-    def read_setup(setupfile):
+    def _read_setup(setupfile):
         """read setup parameters from config file
+        
+        Args:
+            setupfile (str): Path to the setup file (YAML).
+            
+        Returns:
+            dict: containing the setup parameters read from the setupfile.
         """
         with open(setupfile) as file:
             setup_pars = yaml.load(file, Loader=yaml.FullLoader)
@@ -110,6 +129,33 @@ class Dataset:
             quad_pos = [(dx+quad_dict[f"q{x}"][0], dy+quad_dict[f"q{x}"][1]) for x in range(1,5)]
             setup_pars['quadrant_positions'] = quad_pos
             return setup_pars
+        
+    @property
+    def mask(self):
+        """np.ndarray(int): shape(16,512,128) Mask where `bad` pixels are 0
+        and `good` pixels 1.
+        """
+        return self.__mask
+
+    @mask.setter
+    def mask(self, mask):
+        if mask is None:
+            mask = np.ones((16,512,128), 'int8')
+        elif isinstance(mask, str):
+            try:
+                mask = np.load(mask)
+            except Exception as e:
+                print("Loading the mask failed. Error: ", e)
+        elif isinstance(mask, np.ndarray):
+            if mask.shape != (16,512,128):
+                try:
+                    mask = mask.reshape(16,512,128)
+                except Exception as e:
+                    print("Mask array has invalid shape: ", mask.shape)
+        else:
+            raise TypeError(f'Cannot read mask of type {type(mask)}.')
+            
+        self.__mask = np.array(mask).astype('int8')       
     
     
 if __name__ == "__main__":
